8bb54ee0a78f15198a0ae31ced402bf4ca775b86
Mahmoud Mshrf <mahmoudmshrf237@gmail.com>
2024-12-24 22:11:46 +0200

    Add EncapsulateVariation (console app project)

---
+++ EncapsulateVariation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EncapsulateVariation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
