da1c0f7de9d78181cd0a92d222724294d3e6bbd7
Mahmoud Mshrf <mahmoudmshrf237@gmail.com>
2024-12-24 22:41:22 +0200

    Refactor pizza ordering and add specific pizza types
    Corrected namespace declaration and updated Program class.
    Introduced Pizza class with Title and Price properties.
    Refactored OrderPizza method to separate pizza creation.
    Added CreatePizza method for conditional pizza instantiation.
    Added Prepare, Bake, and Cut methods for pizza-making steps.
    Introduced Margherita, Capricciosa, and Calzone subclasses.

--- EncapsulateVariation/Program.cs
+++ EncapsulateVariation/Program.cs
@@ -1,10 +1,108 @@
-﻿namespace EncapsulateVariation
+﻿
+namespace EncapsulateVariation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
         }
+    }
+    public class Pizza 
+    {
+        public virtual string Title => $"{nameof(Pizza)}";
+        public virtual decimal Price => 10m;
+        //public static Pizza OrderPizza(string type)
+        //{
+        //    Pizza pizza;
+        //    if(type == nameof(Margherita))
+        //    {
+        //        pizza= new Margherita();
+        //    }
+        //    else if(type == nameof(Capricciosa))
+        //    {
+        //        pizza= new Capricciosa();
+        //    }
+        //    else
+        //    {
+        //        pizza= new Calzone();
+        //    }
+        //    Prepare();
+
+        //    Bake();
+
+        //    Cut();
+
+        //    return pizza;
+        //}// This method is not a good practice because it is doing more than one thing. It is creating the object and also preparing, baking and cutting the pizza.
+        // We should separate the creation of the object and the preparation, baking and cutting of the pizza.
+
+        public static Pizza OrderPizza(string type)
+        {
+            Pizza pizza = CreatePizza(type);
+            
+            Prepare();
+
+            Bake();
+
+            Cut();
+
+            return pizza;
+        }// This method is a good practice because it is doing only one thing. It is creating the object of the pizza type (by calling CreatePizza) and then preparing, baking and cutting the pizza.
+        // here we have separated the creation of the object and the preparation, baking and cutting of the pizza.
+        // because the creation of the Pizza will be changed frequently because of adding new types of pizza. So, we have separated the creation of the object but the preparation, baking and cutting of the pizza will remain the same.
+        // so in case of any change in the creation of the object, we will change only the CreatePizza method and the rest of the code will remain the same.
+
+        private static Pizza CreatePizza(string type)
+        {
+            Pizza pizza;
+            if (type == nameof(Margherita))
+            {
+                pizza = new Margherita();
+            }
+            else if (type == nameof(Capricciosa))
+            {
+                pizza = new Capricciosa();
+            }
+            else
+            {
+                pizza = new Calzone();
+            }
+            return pizza;
+        }// This method is responsible for creating the object of the pizza type.
+        // This method is doing only one thing. It is creating the object of the pizza type.
+
+        private static void Prepare()
+        {
+            Console.Write("Preparing .....");
+            Thread.Sleep(500);
+            Console.WriteLine("Prepared");
+        }
+        private static void Bake()
+        {
+            Console.Write("Baking .....");
+            Thread.Sleep(500);
+            Console.WriteLine("Baked");
+        }
+        private static void Cut()
+        {
+            Console.Write("Cutting .....");
+            Thread.Sleep(500);
+            Console.WriteLine("Cut");
+        }
+    }
+    public class Margherita : Pizza
+    {
+        public override string Title => $"{nameof(Pizza)} {nameof(Margherita)}";
+        public override decimal Price => base.Price + 5m;
+    }
+    public class Capricciosa : Pizza
+    {
+        public override string Title => $"{nameof(Pizza)} {nameof(Capricciosa)}";
+        public override decimal Price => base.Price + 10m;
+    }
+    public class Calzone : Pizza
+    {
+        public override string Title => $"{nameof(Pizza)} {nameof(Calzone)}";
+        public override decimal Price => base.Price + 15m;
     }
 }
