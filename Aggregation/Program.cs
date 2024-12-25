735efd4d880b87a18e1c16e1a3e84f5fefd06802
Mahmoud Mshrf <mahmoudmshrf237@gmail.com>
2024-12-26 01:16:15 +0200

    Add Aggregation and Composition projects to solution
    Introduce two new projects, `Aggregation` and `Composition`, into the `Design_Principles.sln` solution.
    
    In `Aggregation.csproj`:
    - Create project file with SDK `Microsoft.NET.Sdk`.
    - Configure to output an executable (`Exe`).
    - Set target framework to `.NET 8.0`.
    - Enable implicit usings and nullable reference types.
    
    In `Program.cs` of the `Aggregation` project:
    - Create `Aggregation` namespace.
    - Define `Program` class to demonstrate aggregation with `ClassRoom` and `Student`.
    - Implement `ClassRoom` class to manage multiple `Student` instances.
    - Implement `Student` class with reference to `ClassRoom`.
    
    In `Composition.csproj`:
    - Create project file with SDK `Microsoft.NET.Sdk`.
    - Configure to output an executable (`Exe`).
    - Set target framework to `.NET 8.0`.
    - Enable implicit usings and nullable reference types.
    
    In `Program.cs` of the `Composition` project:
    - Create `Composition` namespace.
    - Define `Program` class to demonstrate composition with `House` and `Room`, and `Car` and `Engine`.
    - Implement `House` class to manage multiple `Room` instances.
    - Implement `Car` class with `Engine` instance and method to start the car.
    
    In `Design_Principles.sln`:
    - Update solution file to include `Aggregation` and `Composition` projects.
    - Add configuration settings for `Debug` and `Release` builds.
    - Nest new projects under `ClassRelationships` solution folder.

---
+++ Aggregation/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Aggregation
+{
+    // Aggregation is a special form of association. It is a relationship between two classes like association, however, it is a directional association, which means it is strictly a one-way association.
+    // It represents a "whole-part" relationship.
+    // In Aggregation, the child class can exist independently of the parent class.
+    // Aggregation is represented by an empty diamond.
+    // Aggregation is a weak relationship.
+    // Aggregation is a relationship where the child can exist independently of the parent.
+    // Let's take an example of a class and students. A class can have multiple students, but a student can only belong to one class.
+    // If a class is deleted, the students will not be deleted.
+    // The class and students are independent entities.
+    // The class can exist without students and students can exist without a class.
+    // The class is not responsible for the creation and destruction of students.
+    // The class and students have their own lifecycle.
+    // The class can have a reference to students.
+    // The student can have a reference to the class.
+    // The class and students are not strongly dependent on each other.
+    // The class and students can be created and deleted independently.
+    // Bidirectional references can lead to circular dependencies, which may complicate memory management and debugging. Use them only when necessary and avoid them in scenarios where simpler unidirectional relationships suffice.
+    // in this example, the student has a reference to the class room just to make the student aware of the class room he belongs to it.
+    // aggregation is unidirectional relationship by default, but you can make it bidirectional by adding a reference to the parent class in the child class but it may lead to circular dependencies, which may complicate memory management and debugging but in this example, it's not a problem and we add it just to make the student aware of the class room he belongs to it.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classRoom = new ClassRoom("ClassRoom1");
+            var student1 = new Student("Student1");
+            var student2 = new Student("Student2");
+            var student3 = new Student("Student3");
+            var students = new List<Student> { student2, student3 };
+            classRoom.AddStudent(student1);
+            classRoom.AddStudents(students);
+            foreach (var student in classRoom.Students)
+            {
+                Console.WriteLine($"{student.Name} belongs to {student.ClassRoom.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+    class ClassRoom
+    {
+        public string Name { get; set; }
+
+        public ClassRoom(string name)
+        {
+            Name = name;
+        }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+            student.ClassRoom = this;
+        }
+        public void AddStudents(List<Student> students)
+        {
+            Students.AddRange(students);
+            foreach (var student in students)
+            {
+                student.ClassRoom = this;
+            }
+        }
+
+    }
+    class Student
+    {
+        public string Name { get; private set; }
+        public ClassRoom ClassRoom { get;  set; }// just to make the student aware of the class room he belongs to it, but it may lead to circular dependencies, which may complicate memory management and debugging so it not preferred to make it bidirectional
+        public Student(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
---
+++ Composition/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace Composition
+{
+    // Composition is a special form of aggregation. It is a relationship between two classes like association, however, it is a strong association.
+    // It represents a "death" relationship.
+    // In Composition, the child class cannot exist independently of the parent class.
+    // Composition is represented by a filled diamond.
+    // Composition is a strong relationship.
+    // Composition is a relationship where the child cannot exist independently of the parent.
+    // Let's take an example of a house and rooms. A house can have multiple rooms, but a room cannot exist without a house.
+    // If a house is deleted, all rooms will be deleted.
+    // The house and rooms are dependent entities.
+    // The house is responsible for the creation and destruction of rooms.
+    // The house can have a reference to rooms.
+    // The room cannot have a reference to the house.
+    // The house and rooms are strongly dependent on each other.
+    // The house and rooms can be created and deleted together.
+    // ************************************************
+    // ************************************************
+    // Car and Engine example
+    // In this example, the car has an engine, and the engine cannot exist without the car.
+    // If the car is deleted, the engine will be deleted.
+    // The car and engine are dependent entities.
+    // The car is responsible for the creation and destruction of the engine.
+    // The car can have a reference to the engine.
+    // The engine cannot have a reference to the car.
+    // The car and engine are strongly dependent on each other.
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House house = new House("123 Main St");
+            house.AddRoom(1);
+            house.AddRoom(2);
+            house.AddRoom(3);
+            foreach (var room in house.Rooms)
+            {
+                Console.WriteLine($"Room {room.RoomNumber} belongs to {house.Address} house");
+            }
+            Console.WriteLine("******************************");
+            Console.WriteLine("******************************");
+            Console.WriteLine("Car and Engine example \n");
+            var car = new Car("Toyota Corolla", "V4");
+            car.StartCar();
+        }
+    }
+    class House
+    {
+        public string Address { get; private set; }
+        public List<Room> Rooms { get; private set; } = new List<Room>();
+        public House(string address)
+        {
+            Address = address;
+        }
+        public void AddRoom(int roomNumber)
+        {
+            Rooms.Add(new Room(roomNumber));// Room is created as part of the house
+        }
+    }
+    class Room
+    {
+        public int RoomNumber { get; private set; }
+        public Room(int roomNumber)
+        {
+            RoomNumber = roomNumber;
+        }
+    }
+    public class Engine
+    {
+        public string Type { get; private set; }
+
+        public Engine(string type)
+        {
+            Type = type;
+        }
+
+        public void Start()
+        {
+            Console.WriteLine($"The {Type} engine is starting...");
+        }
+    }
+
+    public class Car
+    {
+        public string Model { get; private set; }
+        public Engine CarEngine { get; private set; }
+
+        public Car(string model, string engineType)
+        {
+            Model = model;
+            CarEngine = new Engine(engineType); // Engine is created as part of the car
+        }
+
+        public void StartCar()
+        {
+            Console.WriteLine($"{Model} is starting...");
+            CarEngine.Start();
+        }
+    }
+
+}
